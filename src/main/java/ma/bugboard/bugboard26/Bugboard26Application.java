0eba2c0c816ef3c86afc49bb6984f078485013c4
Michele <micheledamato20@hotmail.com>
2025-12-20 15:02:56 +0100

    chore: initial project setup

---
+++ src/main/java/ma/bugboard/bugboard26/Bugboard26Application.java
@@ -0,0 +1,13 @@
+package ma.bugboard.bugboard26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bugboard26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bugboard26Application.class, args);
+	}
+
+}
---
+++ src/test/java/ma/bugboard/bugboard26/Bugboard26ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.bugboard.bugboard26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bugboard26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
